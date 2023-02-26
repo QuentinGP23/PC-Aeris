e174bf9793daa5e585ff19a1b7195f8158e3fb9e
QuentinGP <quentgeoffpro@yahoo.com>
2023-02-26 10:26:59 +0100

    start menu pas ouf

--- js/header.js
+++ js/header.js
@@ -1,5 +1,10 @@
 const menu = document.querySelector('.svg-toggle');
+const menuList = document.querySelector('.menu-nav');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer')
 
 menu.addEventListener('click', () =>{
-    
+    menuList.style.display='block';
+    main.style.display='none';
+    footer.style.display='none';
 })
